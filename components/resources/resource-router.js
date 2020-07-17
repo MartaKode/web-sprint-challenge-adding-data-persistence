ac8e43c75b90914319122806d289a2d97f582e90
Marta Krawczyk <martaalicja113@gmail.com>
2020-07-17 14:03:14 -0400

    uncommented my POST for resources in resource-router

--- components/resources/resource-router.js
+++ components/resources/resource-router.js
@@ -15,28 +15,28 @@ router.get('/', (req, res) => {
 })
 
 // ``````POST````````
-// router.post('/', (req, res) => {
-//     if(!Object.keys(req.body).length){
-//         res.status(400).json({message: 'nothing to post!'})
-//     } else if(!req.body.resource_name){
-//         res.status(400).json({message: 'missing required field resource_name'})
-//     }
+router.post('/', (req, res) => {
+    if(!Object.keys(req.body).length){
+        res.status(400).json({message: 'nothing to post!'})
+    } else if(!req.body.resource_name){
+        res.status(400).json({message: 'missing required field resource_name'})
+    }
 
-//     ResourceModel.addResource(req.body)
-//     .then( newResource => {
-//         res.json(newResource)
-//     })
-//     .catch(err => {
-//         ResourceModel.getResources()
-//         .then( resources => {
-//             if( resources.filter(resource => resource.resource_name === req.body.resource_name)){
-//                 res.status(400).json({message: "resource_name must be unique"})
-//             }else {
-//                 res.status(500).json({error: err.message})
-//             }
-//         })
+    ResourceModel.addResource(req.body)
+    .then( newResource => {
+        res.json(newResource)
+    })
+    .catch(err => {
+        ResourceModel.getResources()
+        .then( resources => {
+            if( resources.filter(resource => resource.resource_name === req.body.resource_name)){
+                res.status(400).json({message: "resource_name must be unique"})
+            }else {
+                res.status(500).json({error: err.message})
+            }
+        })
        
-//     })
-// })
+    })
+})
 
 module.exports = router
